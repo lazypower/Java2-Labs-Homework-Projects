59e8052eae28b5a1a3b5ff49c2721767104262a5
Charles Butler <chuck@dasroot.net>
2012-05-28 20:51:52 -0400

    Lab5b boilerplate checkpoint

---
+++ Lab5b/src/lab5b/Lab5b.java
@@ -0,0 +1,92 @@
+package lab5b;
+
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+        
+
+public class Lab5b extends JApplet implements MemoryListener
+{
+
+    boolean isStandalone = false;
+    private MemoryWatch memoryWatch1 = new MemoryWatch();
+    private JScrollPane jscrollPane1 = new JScrollPane();
+    private JTextArea jTextArea1 = new JTextArea();
+    
+    
+    public void init() 
+    {
+        this.setSize(new Dimension(400,300));
+        jTextArea1.setText("jTextArea1");
+        
+        memoryWatch1.addMemoryListener(new MemoryListener()
+        {
+            public void sufficientMemory(MemoryEvent e) 
+            { }
+
+            public void insufficientMemory(MemoryEvent e) 
+            {
+                memoryWatch1_insufficientMemory(e);
+            }
+        });
+        
+        memoryWatch1.addMemoryListener(new MemoryListener()
+        {
+            public void sufficientMemory(MemoryEvent e)
+            {
+                memoryWatch1_sufficientMemory(e);
+            }
+            
+            public void insufficientMemory(MemoryEvent e)
+            { }
+        });
+        
+        // add jscrollpane to center of applet
+        jscrollPane1.getViewport().add(jTextArea1, null);
+    }
+    
+    public static void main(String[] args) 
+    {
+        Lab5b applet = new Lab5b();
+        applet.isStandalone = true;
+        JFrame frame = new JFrame();
+        frame.setTitle("Lab5b");
+        frame.getContentPane().add(applet, BorderLayout.CENTER);
+        
+        applet.init();
+        applet.start();
+        frame.setSize(400, 320);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    
+    public void sufficientMemory(MemoryEvent e)
+    {
+        jTextArea1.setText("Sufficient Memory");
+        jTextArea1.append("highLimit " + memoryWatch1.getHighLimit()+"\n");
+        jTextArea1.append("free memory " + e.getFreeMemory() + "\n");
+        jTextArea1.append("total memory " + e.getTotalMemory() + "\n");
+        
+    }
+    
+    public void insufficientMemory(MemoryEvent e) 
+    {
+        jTextArea1.setText("Insufficient Memory");
+        jTextArea1.append("highLimit " + memoryWatch1.getHighLimit()+"\n");
+        jTextArea1.append("free memory " + e.getFreeMemory() + "\n");
+        jTextArea1.append("total memory " + e.getTotalMemory() + "\n");
+    }
+    
+    void memoryWatch1_insufficientMemory(MemoryEvent e) 
+    {
+        insufficientMemory(e);
+    }
+    
+    void memoryWatch1_sufficientMemory(MemoryEvent e) 
+    {
+        sufficientMemory(e);
+    }
+    
+}
