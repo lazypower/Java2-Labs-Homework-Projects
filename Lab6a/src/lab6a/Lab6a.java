91672f2f13b9f96468817b55d65ebf975e1d4bbd
Charles Butler <chuck@dasroot.net>
2012-05-29 00:08:50 -0400

    More checkpoints in boilerplate

--- Lab6a/src/lab6a/Lab6a.java
+++ Lab6a/src/lab6a/Lab6a.java
@@ -21,15 +21,38 @@ public class Lab6a extends JApplet implements ActionListener
         
         cardPanel.add(intPanel, "Integer");
         cardPanel.add(rationalPanel, "Rational");
-        cardPanel.setLayout(layout);
         
-        )
+        getContentPane().setLayout(new BorderLayout());
+        add(jcboIntRational, BorderLayout.NORTH);
+        add(cardPanel, BorderLayout.CENTER);
         
+        jcboIntRational.addActionListener(this);
     }
     
+    public void actionPerformed(ActionEvent e)
+    {
+        if (jcboIntRantional.getSelectedItem().equals("Integer Operation"))
+        {
+            //p1074
+        }
+        else if (jcboIntRational.getSelectedItem().equals("Rational Operation"))
+        {
+            //p 1074 flip to last
+        }
+    }
     
-
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        Lab6a applet = new Lab6a();
+        JFrame frame = new JFrame();
+        frame.setTitle(("Lab6a"));
+        frame.getContentPane().add(applet, BorderLayout.CENTER);
+        applet.init();
+        applet.start();
+        
+        frame.setSize(400, 320);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
